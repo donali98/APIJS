a2e1d4f24d46a116b10d8856c6433535cb70e68b
Dona <00084417@uca.edu.sv>
2019-11-22 17:24:29 -0600

    Innitial commit

---
+++ routes/users.js
@@ -0,0 +1,11 @@
+var express = require("express");
+var router = express.Router();
+const UserController = require("../controllers/UserController");
+
+/* GET users listing. */
+router.post("/insert", UserController.insert);
+router.get("/:username", UserController.getOne);
+router.get("/", UserController.getAll);
+router.put("/:username", UserController.update);
+
+module.exports = router;
